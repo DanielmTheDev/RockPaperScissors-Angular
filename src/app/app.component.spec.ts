32c4db275e1cd6efb775969c4285f395b05b5d75
imenehakem <hakem.imene0@gmail.com>
2022-10-10 21:54:16 +0200

    Unit test message

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Rock Scissors Paper Online Game'`, () => {
+  it(`returns 'Rock Scissors Paper Online Game' as title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Rock Scissors Paper Online Game');
