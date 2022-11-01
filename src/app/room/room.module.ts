a7207bd87bd0918b7d11d16a381ba2da53e514ee
Daniel Muckelbauer <dadamuck@hotmail.com>
2022-11-01 09:44:11 +0100

    Add copy link component

--- src/app/room/room.module.ts
+++ src/app/room/room.module.ts
@@ -5,16 +5,21 @@ import { RoomComponent } from './room.component';
 import { PlayerCreationModule } from '../player-creation/player-creation.module';
 import { ChoiceModule } from '../choice/choice.module';
 import { OpponentsModule } from '../opponents/opponents.module';
+import { CopyLinkComponent } from './copy-link/copy-link.component';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @NgModule({
-  declarations: [RoomComponent],
+  declarations: [RoomComponent, CopyLinkComponent],
   imports: [
     CommonModule,
     MatDialogModule,
     PlayerCreationModule,
     OpponentsModule,
-    ChoiceModule
+    ChoiceModule,
+    ClipboardModule
   ],
+  providers: [MatSnackBar],
   exports: [RoomComponent]
 })
 export class RoomModule {}
