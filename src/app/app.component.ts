74fa6a1738a57afeb5f8acd7c9403499f9aed77f
imenehakem <hakem.imene0@gmail.com>
2022-11-18 12:51:18 +0100

    Fix title styling and button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Rock Paper Scissors Online Game';
+  title = 'Rock Paper Scissors';
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RoomCreationModule } from '../room-creation/room-creation.module';
+import { MatButtonModule } from "@angular/material/button";
 
 
 @NgModule({
@@ -9,9 +10,10 @@ import { RoomCreationModule } from '../room-creation/room-creation.module';
   exports: [
     HomeComponent
   ],
-  imports: [
-    BrowserModule,
-    RoomCreationModule
-  ]
+    imports: [
+        BrowserModule,
+        RoomCreationModule,
+        MatButtonModule
+    ]
 })
 export class HomeModule {}
