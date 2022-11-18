c8ef9edf6a1546dc45c6bbd04b014d03a2d095b1
imenehakem <hakem.imene0@gmail.com>
2022-11-18 12:56:14 +0100

    Fix linting ˀ

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,8 +2,7 @@ import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RoomCreationModule } from '../room-creation/room-creation.module';
-import { MatButtonModule } from "@angular/material/button";
-
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [HomeComponent],
