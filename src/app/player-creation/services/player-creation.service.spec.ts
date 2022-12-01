7c89fc4a980e8144237535a7d8a5f58c2dc46c0d
Daniel Muckelbauer <dadamuck@hotmail.com>
2022-12-01 23:14:11 +0100

    Fix something but not all :(

--- src/app/player-creation/services/player-creation.service.spec.ts
+++ src/app/player-creation/services/player-creation.service.spec.ts
@@ -5,19 +5,21 @@ import { FirebasePlayerService } from '../../firebase/services/firebase-player.s
 import { PlayerCreationModalComponent } from '../player-creation-modal/player-creation-modal.component';
 import { of } from 'rxjs';
 import { setPlayer } from '../../store';
-import { Player } from '../../firebase/models/player';
+import { FirebasePlayerInRoomService } from '../../firebase/services/firebase-player-in-room.service';
 
 describe('PlayerCreationService', () => {
   let service: PlayerCreationService;
   let dialog: MatDialog;
   let firebasePlayerService: FirebasePlayerService;
+  let playerInRoomService: FirebasePlayerInRoomService;
   let store: Store;
 
   beforeEach(() => {
     dialog = { open: (_1, _2) => {} } as MatDialog;
     firebasePlayerService = { add: _ => of({}) } as FirebasePlayerService;
+    playerInRoomService = {} as FirebasePlayerInRoomService;
     store = { dispatch: _ => {} } as Store;
-    service = new PlayerCreationService(dialog, firebasePlayerService, store);
+    service = new PlayerCreationService(dialog, firebasePlayerService, playerInRoomService, store);
   });
 
   it('writes player to store', () => {
@@ -28,6 +30,6 @@ describe('PlayerCreationService', () => {
 
     service.createPlayer('roomId');
 
-    expect(store.dispatch).toHaveBeenCalledWith(setPlayer({ player: { name: 'daniel', id: 'playerId', room: 'roomId' } as Player }));
+    expect(store.dispatch).toHaveBeenCalledWith(setPlayer({ playerId: 'playerId', playerInRoomId: 'roomId' }));
   });
 });
