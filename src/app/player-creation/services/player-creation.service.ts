340b06cc5373db8cab54e3f9e15126569ccd9c0d
imenehakem <hakem.imene0@gmail.com>
2022-12-23 17:03:54 +0100

    fix merge conflicts

--- src/app/player-creation/services/player-creation.service.ts
+++ src/app/player-creation/services/player-creation.service.ts
@@ -6,6 +6,8 @@ import { Store } from '@ngrx/store';
 import { setPlayer } from '../../store';
 import { FirebasePlayerService } from '../../firebase/services/firebase-player.service';
 import { PlayerCreationModalComponent } from '../components/player-creation-modal/player-creation-modal.component';
+import { map, Observable, switchMap } from 'rxjs';
+import { AvatarGenerator } from './avatar-generator.service';
 
 @Injectable()
 export class PlayerCreationService {
@@ -13,29 +15,28 @@ export class PlayerCreationService {
   constructor(
     private dialog: MatDialog,
     private playerService: FirebasePlayerService,
+    private generator: AvatarGenerator,
     private store: Store) { }
 
-  createPlayer(roomId: string): void {
+  createPlayer(roomId: string): Observable<void> {
     const dialogRef = this.dialog.open(PlayerCreationModalComponent, {
       width: constants.dialogWidth,
       disableClose: true
     });
-    dialogRef.afterClosed().subscribe(name => this.persistPlayer(name, roomId));
+    return dialogRef.afterClosed().pipe(map(name => this.persistPlayer(name, roomId)));
   }
 
   private persistPlayer(name: string, roomId: string): void {
-    const player = this.initializeBasicPlayer(name, roomId);
-    this.playerService.add(player)
+    this.initializeBasicPlayer(name, roomId)
+      .pipe(switchMap(player => this.playerService.add(player)))
       .subscribe(playerId => {
-        player.id = playerId;
+        const player = { id: playerId } as Player;
         this.store.dispatch(setPlayer({ player: player }));
       });
   }
 
-  private initializeBasicPlayer(name: string, roomId: string): Player {
-    return {
-      name: name,
-      room: roomId
-    } as Player;
+  private initializeBasicPlayer(name: string, roomId: string): Observable<Player> {
+    return this.generator.provide(name)
+      .pipe(map(avatar => { return { name: name, room: roomId, avatar: avatar } as Player; }));
   }
 }
--- src/app/room/components/room.component.ts
+++ src/app/room/components/room.component.ts
@@ -6,10 +6,8 @@ import { Store } from '@ngrx/store';
 import { CurrentPlayer } from 'src/app/store/models/current-player';
 import { Player } from 'src/app/firebase/models/player';
 import { LoadingStatus } from 'src/app/shared-lib/spinner/models/loadingStatus';
-import { PlayerInRoom } from 'src/app/firebase/models/playerInRoom';
 import { PlayerCreationService } from 'src/app/player-creation/services/player-creation.service';
 import { FirebasePlayerService } from 'src/app/firebase/services/firebase-player.service';
-import { FirebasePlayerInRoomService } from 'src/app/firebase/services/firebase-player-in-room.service';
 import { selectPlayer } from 'src/app/store/state';
 import constants from 'src/app/constants';
 import { removePlayer } from 'src/app/store';
@@ -23,27 +21,27 @@ export class RoomComponent implements OnInit {
   currentPlayer$: Observable<CurrentPlayer>;
   firebasePlayer$: Observable<Player | undefined> | undefined;
   loadingStatus: LoadingStatus = { isLoading: false };
-  firebasePlayerInRoom$: Observable<PlayerInRoom | undefined>;
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
     private playerCreationService: PlayerCreationService,
     private firebasePlayerService: FirebasePlayerService,
-    private playerInRoomService: FirebasePlayerInRoomService,
     private route: ActivatedRoute,
     private store: Store) {
     this.currentPlayer$ = store.select(selectPlayer);
     this.firebasePlayerService.getCurrentPlayerDocument().subscribe(player => {
       this.firebasePlayer$ = player.valueChanges();
     });
-    this.firebasePlayerInRoom$ = this.playerInRoomService.getCurrentPlayerInRoomDocument();
   }
 
   ngOnInit(): void {
+    this.loadingStatus.isLoading = true;
     this.currentPlayer$.pipe(take(1)).subscribe(player => {
-      if (!player.playerId) {
-        this.playerCreationService.createPlayer(this.route.snapshot.params[constants.routeParams.id]);
+      if (!player.id) {
+        this.playerCreationService.createPlayer(this.route.snapshot.params[constants.routeParams.id]).subscribe(_ => this.loadingStatus.isLoading = false);
+      } else {
+        this.loadingStatus.isLoading = false;
       }
     });
   }
@@ -52,13 +50,13 @@ export class RoomComponent implements OnInit {
     this.loadingStatus.isLoading = true;
     this.currentPlayer$.pipe(take(1))
       .pipe(
-        map(player => this.firebasePlayerService.remove(player.playerId)),
+        map(player => this.firebasePlayerService.remove(player.id)),
         finalize(() => this.loadingStatus.isLoading = false))
       .subscribe(
-      () => {
+        () => {
           this.store.dispatch(removePlayer());
           this.router.navigate(['']).then();
         }
-    );
+      );
   }
 }
