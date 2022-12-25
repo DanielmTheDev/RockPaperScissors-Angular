f885af42eb39c2c2827e28c5741ad059894c6b60
Daniel Muckelbauer <dadamuck@hotmail.com>
2022-12-25 23:34:05 +0100

    WIP

--- firebase-functions/functions/.eslintrc.js
+++ firebase-functions/functions/.eslintrc.js
@@ -27,5 +27,7 @@ module.exports = {
   rules: {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
+    "max-len": ["error", {"code": 150}],
+    "@typescript-eslint/no-var-requires": 0,
   },
 };
--- firebase-functions/functions/src/index.ts
+++ firebase-functions/functions/src/index.ts
@@ -1,44 +1,41 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp();
-/*
-exports.addResultOnRoomCreation = functions.firestore.document('/rooms/{documentId}')
-    .onCreate(async (snap, context) => {
-        const roomId = context.params.documentId;
-        await admin.firestore().collection('results')
-            .add({ id: roomId, winner: 'daniel as usual!' });
+exports.addResultOnRoomCreation = functions.firestore.document("/rooms/{documentId}")
+    .onCreate(async (snap: any, context: any) => {
+      const roomId = context.params.documentId;
+      await admin.firestore().collection("results")
+          .add({id: roomId, winner: "daniel as usual!"});
     });
-*/
 
-exports.choiceUpdated = functions.firestore.document('/players/{documentId}')
-  .onUpdate((change, context) => {
+exports.choiceUpdated = functions.firestore.document("/players/{documentId}")
+    .onUpdate((change: any, context: any) => {
+      const playerId = context.params.documentId;
+      admin.firestore().collection("players").doc(playerId).get()
+          .then((doc: any) => {
+            // get the room where the player is
+            const roomId = doc.data().room;
+            console.log("The room in question " + roomId);
 
-    const playerId = context.params.documentId;
-    admin.firestore().collection('players').doc(playerId).get()
-      .then(doc => {
-        // get the room where the player is
-        const roomId = doc.data().room;
-        console.log('The room in question ' + roomId);
+            // get all the players in the same room
+            admin.firestore().collection("players").where("room", "==", roomId).get()
+                .then((snapshot: any) => {
+                  const playersWithChoices = [];
+                  let areAllChoicesPresent = true;
 
-        // get all the players in the same room
-        admin.firestore().collection('players').where("room", "==", roomId).get()
-          .then(snapshot => {
-            const playersWithChoices = [];
-            let areAllChoicesPresent = true;
+                  snapshot.forEach((player: any) => {
+                    if (!player.data().choices) {
+                      areAllChoicesPresent = false;
+                      return;
+                    }
+                    playersWithChoices.push(player.data());
+                  });
+                  console.log("should we set a result? " + areAllChoicesPresent);
 
-            snapshot.forEach(player => {
-              if (!player.data().choices) {
-                areAllChoicesPresent = false;
-                return;
-              }
-              playersWithChoices.push(player.data());
-            });
-            console.log("should we set a result? " + areAllChoicesPresent);
-
-            if(areAllChoicesPresent) {
-              admin.firestore().collection('rooms').doc(roomId).update({ winner: 'daniel as usual!' });
-            }
-          })
-      });
-  });
+                  if (areAllChoicesPresent) {
+                    admin.firestore().collection("rooms").doc(roomId).update({winner: "daniel as usual!"});
+                  }
+                });
+          });
+    });
 
