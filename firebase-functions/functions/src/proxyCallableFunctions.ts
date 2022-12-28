6747ef1e63a074992193d080bbf71326a56ff60c
Daniel Muckelbauer <dadamuck@hotmail.com>
2022-12-28 09:04:22 +0100

    Get avatar over callable firebase function to avoid CORS and rate limit restrictions

---
+++ firebase-functions/functions/src/proxyCallableFunctions.ts
@@ -0,0 +1,9 @@
+﻿import * as functions from 'firebase-functions';
+
+export default functions.https.onCall(async data => {
+  const url = 'https://api.multiavatar.com/';
+  const response = await fetch(`${url}${data.playerName}?apikey=8IPOBEe7WmXA26`);
+  return await response.text();
+});
+
+// next up: same thing for random names
--- src/app/player-creation/services/avatar-generator.service.ts
+++ src/app/player-creation/services/avatar-generator.service.ts
@@ -1,20 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AngularFireFunctions } from '@angular/fire/compat/functions';
 
 @Injectable()
 export class AvatarGenerator {
-  private readonly url = 'https://proxy.cors.sh/https://api.multiavatar.com/';
-
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient, private functions: AngularFireFunctions) {}
 
   provide(playerName: string): Observable<string> {
-    const HTTPOptions = {
-      'headers': new HttpHeaders({
-        'Accept': 'text/html'
-      }),
-      'responseType': 'text' as 'json'
-    };
-    return this.httpClient.get<string>(this.url + playerName, HTTPOptions);
+     return this.functions.httpsCallable('getAvatar')({ playerName });
   }
 }
