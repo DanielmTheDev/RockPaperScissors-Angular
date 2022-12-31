f030312a95793e8015e74505ef0a082606c2e297
Daniel Muckelbauer <dadamuck@hotmail.com>
2022-12-31 16:50:46 +0100

    Import types correctly

--- firebase-functions/functions/src/calculateResult.ts
+++ firebase-functions/functions/src/calculateResult.ts
@@ -1,15 +1,12 @@
 ﻿import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import { firestore } from 'firebase-admin';
 import { Player } from './models/player';
 import { getLosers, hasEveryoneChosen } from './choiceOperations';
 import { Room } from './models/room';
 import { GameType } from './models/game-type';
-
-async function getCurrentGameType(roomId: string): Promise<GameType | undefined> {
-  const room = await admin.firestore().collection('rooms').doc(roomId).get();
-  console.log(JSON.stringify(room.data()));
-  return (room.data() as Room).typeOfGame;
-}
+import QueryDocumentSnapshot = firestore.QueryDocumentSnapshot;
+import DocumentData = firestore.DocumentData;
 
 export const calculateResult = functions.firestore.document('/players/{documentId}')
   .onUpdate(async (change: any, context: any) => {
@@ -32,9 +29,13 @@ export const calculateResult = functions.firestore.document('/players/{documentI
     }
   });
 
-async function determineWinner(
-  playersInSameRoom: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[],
-  losers: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[],
+async function getCurrentGameType(roomId: string): Promise<GameType | undefined> {
+  const room = await admin.firestore().collection('rooms').doc(roomId).get();
+  console.log(JSON.stringify(room.data()));
+  return (room.data() as Room).typeOfGame;
+}
+
+async function determineWinner(playersInSameRoom: QueryDocumentSnapshot<DocumentData>[], losers: QueryDocumentSnapshot<DocumentData>[],
   roomId: string): Promise<void> {
   const winners = playersInSameRoom.filter(player => !losers.includes(player));
   if (winners.length === 1) {
@@ -43,7 +44,7 @@ async function determineWinner(
   }
 }
 
-async function getActivePlayersInRoom(roomId: string): Promise<FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[]> {
+async function getActivePlayersInRoom(roomId: string): Promise<QueryDocumentSnapshot<DocumentData>[]> {
   return (await admin.firestore().collection('players').where('room', '==', roomId).get())
     .docs.filter(playerDoc => !(playerDoc.data() as Player).isObserver);
 }
@@ -53,6 +54,6 @@ async function getCurrentRoomId(playerId: string): Promise<string> {
   return currentPlayer.data()?.room;
 }
 
-function updatePlayers(players: FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[], updateData: Player): Promise<Awaited<FirebaseFirestore.WriteResult | undefined>[]> {
+function updatePlayers(players: QueryDocumentSnapshot<DocumentData>[], updateData: Player): Promise<Awaited<FirebaseFirestore.WriteResult | undefined>[]> {
   return Promise.all(players.map(playerDoc => admin.firestore().collection('players').doc(playerDoc.id)?.update(updateData)));
 }
--- firebase-functions/functions/src/choiceOperations.ts
+++ firebase-functions/functions/src/choiceOperations.ts
@@ -1,21 +1,24 @@
 ﻿import { Player } from './models/player';
 import { Choice } from './models/choice';
+import { firestore } from 'firebase-admin';
+import QueryDocumentSnapshot = firestore.QueryDocumentSnapshot;
+import DocumentData = firestore.DocumentData;
 
-export function hasEveryoneChosen(players: Array<FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>>): boolean {
+export function hasEveryoneChosen(players: QueryDocumentSnapshot<DocumentData>[]): boolean {
   return players.every(player => (player.data() as Player).choice);
 }
 
-export function getLosers(players: Array<FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>>): FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[] {
+export function getLosers(players: QueryDocumentSnapshot<DocumentData>[]): QueryDocumentSnapshot<DocumentData>[] {
   return isDraw(players)
     ? []
     : getLosersInner(players);
 }
 
-function isDraw(players: Array<FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>>): boolean {
+function isDraw(players: QueryDocumentSnapshot<DocumentData>[]): boolean {
   return isEveryChoiceEqual(players) || isEveryChoiceDifferent(players);
 }
 
-function getLosersInner(players: Array<FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>>): FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>[] {
+function getLosersInner(players: QueryDocumentSnapshot<DocumentData>[]): QueryDocumentSnapshot<DocumentData>[] {
   const allChoices = getDistinctChoices(players);
   if (allChoices.includes(Choice.Rock) && allChoices.includes(Choice.Scissors)) {
     return players.filter(player => (player.data() as Player).choice === 'Scissors');
@@ -26,16 +29,16 @@ function getLosersInner(players: Array<FirebaseFirestore.QueryDocumentSnapshot<F
   }
 }
 
-function getDistinctChoices(players: Array<FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>>): Choice[] {
+function getDistinctChoices(players: QueryDocumentSnapshot<DocumentData>[]): Choice[] {
   return players
     .map(playerDoc => (playerDoc.data() as Player).choice as Choice)
     .filter((value, index, array) => array.indexOf(value) === index);
 }
 
-function isEveryChoiceEqual(players: Array<FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>>): boolean {
+function isEveryChoiceEqual(players: QueryDocumentSnapshot<DocumentData>[]): boolean {
   return getDistinctChoices(players).length === 1;
 }
 
-function isEveryChoiceDifferent(players: Array<FirebaseFirestore.QueryDocumentSnapshot<FirebaseFirestore.DocumentData>>): boolean {
+function isEveryChoiceDifferent(players: QueryDocumentSnapshot<DocumentData>[]): boolean {
   return getDistinctChoices(players).length === 3;
 }
