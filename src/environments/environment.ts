0574c356ea84ae35c807126e5bb6f629910ce9a6
DanielMuckelbauer <dadamuck@gmail.com>
2023-01-11 15:04:58 +0100

    remove environment config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'AIzaSyBCP8GQf4Dsfjl8WcjO2Isz6Sw-yDN90uI',
-    authDomain: 'rockpaperscissors-de233.firebaseapp.com',
-    projectId: 'rockpaperscissors-de233',
-    storageBucket: 'rockpaperscissors-de233.appspot.com',
-    messagingSenderId: '428896464809',
-    appId: '1:428896464809:web:b08eee41a481ff88f11eb8',
-    measurementId: 'G-KN73RXMKZX'
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
   },
-  production: true
+  production: false
 };
