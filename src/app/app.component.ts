9ca8d8dde55912145fc5ea3592c33884dee8da86
imenehakem <hakem.imene0@gmail.com>
2023-01-19 17:29:00 +0100

    fix comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Rock Paper Scissors';
+
+  get currentYear(): number {
+    return (new Date()).getFullYear();
+  }
 }
