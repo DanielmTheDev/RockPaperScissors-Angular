f26642ad798fec1626ad10edcbb4cf52de4a2321
imenehakem <hakem.imene0@gmail.com>
2023-02-05 19:52:16 +0100

    convert dropdown to two buttons

--- src/app/room-creation/room-creation.module.ts
+++ src/app/room-creation/room-creation.module.ts
@@ -1,28 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatRadioModule } from '@angular/material/radio';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RoomCreationModalComponent } from './components/room-creation-modal/room-creation-modal.component';
-import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [RoomCreationModalComponent],
   imports: [
     CommonModule,
-    MatRadioModule,
     MatButtonModule,
+    BrowserAnimationsModule,
     MatDialogModule,
-    MatFormFieldModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    MatRadioModule,
     HttpClientModule,
-    MatSelectModule
   ],
   providers: [],
   exports: [RoomCreationModalComponent]
