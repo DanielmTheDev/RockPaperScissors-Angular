e606585686b89b0f8e687c2d040238664db5b2b0
imenehakem <hakem.imene0@gmail.com>
2023-02-05 19:52:16 +0100

    removed unused import

--- src/app/room-creation/room-creation.module.ts
+++ src/app/room-creation/room-creation.module.ts
@@ -4,14 +4,12 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
 import { RoomCreationModalComponent } from './components/room-creation-modal/room-creation-modal.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [RoomCreationModalComponent],
   imports: [
     CommonModule,
     MatButtonModule,
-    BrowserAnimationsModule,
     MatDialogModule,
     HttpClientModule,
   ],
